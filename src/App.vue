27e4de83e4f5fafa8a4f75645b5a59217cf2966e
mertsincan <sincan.mert@gmail.com>
2019-05-30 12:07:57 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/primevue-logo.png">
+    <HelloWorld msg="Welcome to Your PrimeVue + TypeScript App"/>
+    <form @submit.prevent="greet">
+      <InputText type="text" v-model="text"/>
+      <Button type="submit" label="Submit"/>
+      <h3>{{message}}</h3>
+    </form>
+    <Toast/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from './components/HelloWorld.vue';
+
+@Component({
+  components: {
+    HelloWorld,
+  },
+})
+export default class App extends Vue {
+  public message: string = '';
+  public text: string = '';
+
+  public greet() {
+    if (this.text.length) {
+      this.$toast.add({ severity: 'info', summary: 'Hello ' + this.text });
+      this.message = 'Hello ' + this.text;
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+body #app .p-button {
+  margin-left: .2em;
+}
+
+form {
+  margin-top: 2em;
+}
+</style>
